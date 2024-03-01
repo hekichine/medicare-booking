c4eca9fe19c3ffda813b094688b44d2fefdb7d4b
chinpanik <chienlx1005@gmail.com>
2024-03-01 23:09:26 +0700

    update header

--- frontend/clinic/tailwind.config.js
+++ frontend/clinic/tailwind.config.js
@@ -1,23 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        buttonBgColor: "#0067ff",
+      colors: {
+        primaryColor: "#0067ff",
         yellowColor: "#FEB60D",
         purpleColor: "#9771FF",
         irisBlurColor: "#01b5c5",
         headingColor: "#181a1e",
-        textColor: "#4e545f"
+        textColor: "#4e545f",
       },
-      boxShadow:{
-        panelShadow: "rgba(17,12,46,0.15) 0px 48px 100px 0px;"
-      }
+      boxShadow: {
+        panelShadow: "rgba(17,12,46,0.15) 0px 48px 100px 0px;",
+      },
     },
   },
   plugins: [],
-}
+};
